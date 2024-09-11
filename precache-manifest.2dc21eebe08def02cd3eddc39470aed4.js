a7bbf414d760dbc6c7e25534bb70c94ed01307a8
sanskriti-singh47 <sanskritisingh47@gmail.com>
2024-09-11 16:19:09 +0530

    Updates

---
+++ precache-manifest.2dc21eebe08def02cd3eddc39470aed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ec2a56080263c72f85a23dc98a9afe",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "ef54b2868d07c7460958",
+    "url": "/TextUtils/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/TextUtils/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/TextUtils/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "ef54b2868d07c7460958",
+    "url": "/TextUtils/static/js/main.4f9495e6.chunk.js"
+  },
+  {
+    "revision": "addae886a4d582bb8f4d",
+    "url": "/TextUtils/static/js/runtime~main.b9ff5895.js"
+  }
+]);
